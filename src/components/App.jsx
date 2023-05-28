6c65fba39106201d82f3b98bbe1dc936dab370f9
tkulagina <tkulagina1974@gmail.com>
2023-05-28 21:31:42 +0300

    add profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
-import user from './path/user.json';
-import data from './path/data.json';
-import friends from './path/friends.json';
-import transactions from './path/transactions.json';
+import user from 'path/user.json';
+import data from 'path/data.json';
+import friends from 'path/friends.json';
+import transactions from 'path/transactions.json';
 
-
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList} from './FriendList/FriendList'
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () =>{
   return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,32 +1,34 @@
-import user from './path/user.json';
+import user from '../../path/user.json';
+import PropTypes from 'prop-types';
 
 
-export const data = ( {username, tag, location, avatar, stats}) => {
+export const Profile = ( {username, tag, location, avatar, stats}) => {  
 return (<div class="profile">
   <div class="description">
     <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+      src={avatar}
       alt="User avatar"
       class="avatar"
     />
     <p class="name">{username}</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
+    <p class="tag">{tag}</p>
+    <p class="location">{location}</p>
   </div>
 
   <ul class="stats">
     <li>
       <span class="label">Followers</span>
-      <span class="quantity">1000</span>
+      <span class="quantity">{stats.followers}</span>
     </li>
     <li>
       <span class="label">Views</span>
-      <span class="quantity">2000</span>
+      <span class="quantity">{stats.views}</span>
     </li>
     <li>
       <span class="label">Likes</span>
-      <span class="quantity">3000</span>
+      <span class="quantity">{stats.likes}</span>
     </li>
   </ul>
 </div>)
-}
+};
+
