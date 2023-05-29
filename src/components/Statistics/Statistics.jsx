d8db04d81ff4ff4a849e91b353bb1d720b1c1549
tkulagina <tkulagina1974@gmail.com>
2023-05-29 21:28:34 +0300

    add statistic and transaction

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,26 @@
+import PropTypes from 'prop-types';
 
-export const Statistics = () => {
-    
+export const Statistics = ({title, stats}) => {
+    return <section className="statistics">
+    {title && <h2 className="title">{title}</h2>}
+  
+    <ul className="stat-list">
+      {stats.map((stat => 
+      <li className="item" key={stat.id}>
+      <span className="label">{stat.label}</span>
+      <span className="percentage">{stat.percentage}%</span>
+    </li>))}
+    </ul>
+  </section>
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string,
+      percentage: PropTypes.number
+    }),
+  ).isRequired 
 }
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,4 +1,34 @@
+import PropTypes from 'prop-types';
 
-export const TransactionHistory = () => {
-    
+
+export const TransactionHistory = ({transactions}) => {
+  return <table className="transaction-history">
+  <thead>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    {transactions.map(({id, type , amount, currency}) => {
+      return <tr key = {id}>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
+    </tr>
+    })}
+  </tbody>
+</table>
 }
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.exact({
+        id: PropTypes.string,
+        type: PropTypes.string,
+        amount: PropTypes.string,
+        currency:PropTypes.string.isRequired,
+    }))
+};
