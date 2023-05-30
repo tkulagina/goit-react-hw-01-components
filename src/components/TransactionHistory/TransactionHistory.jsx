afbb794cec9e7cf07dd1b872fa4681c9d7ff870a
tkulagina <tkulagina1974@gmail.com>
2023-05-30 19:16:41 +0300

    add profile style

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,26 +1,31 @@
 import PropTypes from 'prop-types';
 
 
-export const TransactionHistory = ({transactions}) => {
-  return (<table className="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+export const TransactionHistory = (props) => {
+  console.log (props)
+  return (
+  <table className="transaction-history">
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
 
-  <tbody>
-    {transactions.map(({id, type , amount, currency}) => {
-      return <tr key = {id}>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
+    <tbody>
+
+    {props.map(({id, type , amount, currency}) => {
+      return (
+      <tr key = {id}>
+        <td>{type}</td>
+        <td>{amount}</td>
+        <td>{currency}</td>
     </tr>
-    })}
-  </tbody>
-</table>
+    )}
+    )}
+    </tbody>
+  </table>
 )}
 
 TransactionHistory.propTypes = {
